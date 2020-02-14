c7941b0e3fb767db11826010c837c561a8a72472
BoD <BoD@JRAF.org>
2020-02-14 09:31:15 +0100

    Repro steps for "onAnimationEnd called twice when system animations are disabled" issue

--- issue-repro/src/main/java/com/airbnb/lottie/issues/MainActivity.kt
+++ issue-repro/src/main/java/com/airbnb/lottie/issues/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.airbnb.lottie.issues
 
-import android.content.Intent
+import android.animation.Animator
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityOptionsCompat
-import com.airbnb.lottie.issues.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.airbnb.lottie.LottieAnimationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var counter = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // Reproduce any issues here.
+
+        val lottieAnimationView = findViewById<LottieAnimationView>(R.id.lottieAnimationView)
+        lottieAnimationView.setAnimation(R.raw.lottie_logo1)
+
+        //
+        // IMPORTANT NOTE: TO REPRO THIS ISSUE, YOU MUST DISABLE SYSTEM ANIMATIONS:
+        //
+        // adb shell settings put global window_animation_scale 0
+        // adb shell settings put global transition_animation_scale 0
+        // adb shell settings put global animator_duration_scale 0
+        val animatorListener = object : Animator.AnimatorListener {
+            override fun onAnimationEnd(animation: Animator) {
+                // You will notice in the logs that this is called twice
+                Log.d("MainActivity", "onAnimationEnd ${counter++}")
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {}
+            override fun onAnimationCancel(animation: Animator) {}
+            override fun onAnimationStart(animation: Animator) {}
+        }
+        lottieAnimationView.addAnimatorListener(animatorListener)
+        lottieAnimationView.playAnimation()
     }
 }
